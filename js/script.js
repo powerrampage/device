c160cb9e502c4ccc9d6825b766b07a06627713fd
power <64687619+powerrampage@users.noreply.github.com>
2020-08-27 10:43:12 +0500

    device-website

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function MyFunction() {
+    var x = document.getElementById("menu");
+
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
